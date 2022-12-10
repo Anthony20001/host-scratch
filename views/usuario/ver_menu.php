48de80f9aa255a0e2d2f968c1e468025cbcdf49d
Anthony20001 <ledezmaanthony621@gmail.com>
2022-12-10 11:25:34 -0500

    Se añadió la vista para ver menú y la de editar datos de la cuenta

---
+++ views/usuario/ver_menu.php
@@ -0,0 +1,154 @@
+<div class="section bg-light" id="section-menu" data-aos="fade-up">
+	<section class="hero-wrap hero-wrap-2 img_bg" style="background-image: url('images/slider-1.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+
+		<div class="container">
+			<div class="row no-gutters slider-text align-items-end justify-content-center">
+				<div class="col-md-9 ftco-animate text-center">
+					<h1 style="padding-top: 4rem; padding-bottom: 4rem;">
+						<img src="images/img_1.jpg" style="width: 100px; height: 100px;">
+						<span class="mb-2 bread"><?php echo "Fonda la Universidad" ?></span>
+					</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="ftco-section">
+		<div class="container-fluid px-4">
+			<div class="row justify-content-center mb-5 pb-2">
+			</div>
+			<div class="row">
+				<?php //foreach ($listaCat as $categoria) { ?>
+				<div class="col-md-6 col-lg-4 menu-wrap">
+					<div class="heading-menu text-center ftco-animate">
+						<h3><?php echo "Desayunos Ligth" ?></h3>
+					</div>
+					<?php //foreach ($listaPlat as $plato)
+					//if ((new MongoDB\BSON\ObjectId($plato->_id_categoria)) == $categoria["_id"]) { 
+					?>
+
+					<div class="card mb-3" style="max-width: 540px;">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<img src="images/img_1.jpg" class="img-fluid rounded-start" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="menus d-flex ftco-animate">
+						<div class="menu-img  img" style="background-image: url(<?php echo "images/img_2.jpg" ?>);"></div>
+						<div class="text">
+							<div class="d-flex">
+								<div class="one-half">
+									<h3><?php echo "Nombre del plato" ?></h3>
+								</div>
+								<div class="one-forth">
+									<span class="price">$<?php echo "$3.99" ?></span>
+								</div>
+							</div>
+							<p><?php echo "Sandwich de jamón y queso cheedar, tostado, y con queso crema" ?></p>
+						</div>
+					</div>
+					<?php //} 
+					?>
+				</div>
+				<?php //} ?>
+
+				<?php //foreach ($listaCat as $categoria) { ?>
+				<div class="col-md-6 col-lg-4 menu-wrap">
+					<div class="heading-menu text-center ftco-animate">
+						<h3><?php echo "Desayunos Ligth" ?></h3>
+					</div>
+					<?php //foreach ($listaPlat as $plato)
+					//if ((new MongoDB\BSON\ObjectId($plato->_id_categoria)) == $categoria["_id"]) { 
+					?>
+
+					<div class="card mb-3" style="max-width: 540px;">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<img src="images/img_1.jpg" class="img-fluid rounded-start" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="menus d-flex ftco-animate">
+						<div class="menu-img  img" style="background-image: url(<?php echo "images/img_2.jpg" ?>);"></div>
+						<div class="text">
+							<div class="d-flex">
+								<div class="one-half">
+									<h3><?php echo "Nombre del plato" ?></h3>
+								</div>
+								<div class="one-forth">
+									<span class="price">$<?php echo "$3.99" ?></span>
+								</div>
+							</div>
+							<p><?php echo "Sandwich de jamón y queso cheedar, tostado, y con queso crema" ?></p>
+						</div>
+					</div>
+					<?php //} 
+					?>
+				</div>
+				<?php //} ?>
+
+				<?php //foreach ($listaCat as $categoria) { ?>
+				<div class="col-md-6 col-lg-4 menu-wrap">
+					<div class="heading-menu text-center ftco-animate">
+						<h3><?php echo "Desayunos Ligth" ?></h3>
+					</div>
+					<?php //foreach ($listaPlat as $plato)
+					//if ((new MongoDB\BSON\ObjectId($plato->_id_categoria)) == $categoria["_id"]) { 
+					?>
+
+					<div class="card mb-3" style="max-width: 540px;">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<img src="images/img_1.jpg" class="img-fluid rounded-start" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="menus d-flex ftco-animate">
+						<div class="menu-img  img" style="background-image: url(<?php echo "images/img_2.jpg" ?>);"></div>
+						<div class="text">
+							<div class="d-flex">
+								<div class="one-half">
+									<h3><?php echo "Nombre del plato" ?></h3>
+								</div>
+								<div class="one-forth">
+									<span class="price">$<?php echo "$3.99" ?></span>
+								</div>
+							</div>
+							<p><?php echo "Sandwich de jamón y queso cheedar, tostado, y con queso crema" ?></p>
+						</div>
+					</div>
+					<?php //} 
+					?>
+				</div>
+				<?php //} ?>
+			</div>
+	</section>
+</div>
